f173042979bc402300481cb6963e797f5330b9fd
Beau Vandervort <beauvandervort55@gmail.com>
2022-11-16 14:13:39 -0800

    added signup event listener

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ const signInPassword = document.getElementById('sign-in-password');
 const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
+
+redirectIfLoggedIn();
+
+signUpForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const user = await signupUser(signUpEmail.value, signUpPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
