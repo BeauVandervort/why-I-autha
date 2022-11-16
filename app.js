16b52d2f6739d2118927bfdc52ea65a160504d14
Beau Vandervort <beauvandervort55@gmail.com>
2022-11-16 14:06:18 -0800

    app js elements added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+import { redirectIfLoggedIn, signInUser, signupUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
-
-// Wire up sign in and sign up forms to supabase
-// Redirect to /other-page on successful auth
-// Redirect to /other-page when page loads if user is authenticated
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
